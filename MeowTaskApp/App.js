0a7a1aa56e2b85a8b0e26228c89d4defdac47c85
KevinGuga <62311212+KevinGuga@users.noreply.github.com>
2020-03-17 21:08:39 -0300

    Update App.js

--- MeowTaskApp/App.js
+++ MeowTaskApp/App.js
@@ -8,10 +8,21 @@ import {
 } from 'react-native';
 
 export default function App() {
+
+  constructor(prop){
+    super(prop);
+    this.state = {
+      nome: "Miranda",
+      frase: "Quem espera alcança!",
+      alimento: "Abacaxi",
+    }
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar hidden={true}/>
       <Text>Meow Task é o melhor!</Text>
+      <Text>this.state.nome</Text>
     </View>
   );
 }
