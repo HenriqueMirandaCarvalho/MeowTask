43a6f9c584ef181b9297827338fc200d19deea4a
Henrique Carvalho <trafalgar.pao@gmail.com>
2020-03-28 18:02:43 -0300

    a

--- MeowTaskApp/App.js
+++ MeowTaskApp/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { openDatabase } from 'react-native-sqlite-storage';
+var db = openDatabase({ name: 'antares.db' });
 
 import TelaInicio from './pages/TelaInicio';
 import TelaTeste from './pages/TelaTeste';
+
+db.transaction(function(txn) {
+  txn.executeSql(
+    "SELECT firstaccess FROM system WHERE systemid = 1",
+    [],
+    function (tx, res) {
+      const rows = result.rows;
+    }
+  );
+});
 
 const App = createStackNavigator({
   TelaInicio: {
