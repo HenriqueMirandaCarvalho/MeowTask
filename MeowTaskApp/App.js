0385b08f0093313db8d7f55d3a454c7414f070c6
Henrique Carvalho <trafalgar.pao@gmail.com>
2020-03-28 15:33:53 -0300

    Added React Navigation

--- MeowTaskApp/App.js
+++ MeowTaskApp/App.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ImageBackground,
-  Text,
-  TextInput,
-  View,
-  StatusBar
-} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default class App extends React.Component{
+import TelaInicio from './pages/TelaInicio';
 
-  constructor(props){
-    super(props);
-    this.state = {
-      nome: "Miranda",
-      frase: "Quem espera sempre alcança!",
-      alimento: "alimento",
-      imagem: {uri: "https://reactjs.org/logo-og.png"}
-    }
-  }
-
-  render(){
-    return (
-      <View style={styles.container}>
-        <StatusBar hidden={true}/>
-        <ImageBackground source={this.state.imagem}>
-          <Text>Meow Task é o melhor!</Text>
-          <Text>Olá, {this.state.nome}</Text>
-          <Text>Lembre-se: {this.state.frase}</Text>
-          <Text>Coma muitos {this.state.nome} para evitar o coronga</Text>
-        </ImageBackground>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eae6da'
-  }
+const App = createStackNavigator({
+  TelaInicio: {
+    screen: TelaInicio,
+    navigationOptions: {
+      title: 'Tela Inicio',
+      headerStyle: { backgroundColor: '#f05555' },
+      headerTintColor: '#ffffff'
+    },
+  },
 });
+
+export default createAppContainer(App);
---
+++ MeowTaskApp/pages/TelaInicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TelaInicio extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <Text>TESTE</Text>
+            </View>
+        );
+    }
+}
