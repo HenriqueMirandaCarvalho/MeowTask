9351f4466962aa145518a5c547ba4d2f43da682a
KevinGuga <62311212+KevinGuga@users.noreply.github.com>
2020-03-28 14:54:52 -0300

    Update App.js

--- MeowTaskApp/App.js
+++ MeowTaskApp/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   StyleSheet,
+  ImageBackground,
   Text,
   TextInput,
   View,
@@ -14,7 +15,8 @@ export default class App extends React.Component{
     this.state = {
       nome: "Miranda",
       frase: "Quem espera sempre alcança!",
-      alimento: "alimento"
+      alimento: "alimento",
+      imagem: {uri: "https://reactjs.org/logo-og.png"}
     }
   }
 
@@ -22,10 +24,12 @@ export default class App extends React.Component{
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}/>
-        <Text>Meow Task é o melhor!</Text>
-        <Text>Olá, {this.state.nome}</Text>
-        <Text>Lembre-se: {this.state.frase}</Text>
-        <Text>Coma muitos {this.state.nome} para evitar o coronga</Text>
+        <ImageBackground source={this.state.imagem}>
+          <Text>Meow Task é o melhor!</Text>
+          <Text>Olá, {this.state.nome}</Text>
+          <Text>Lembre-se: {this.state.frase}</Text>
+          <Text>Coma muitos {this.state.nome} para evitar o coronga</Text>
+        </ImageBackground>
       </View>
     );
   }
