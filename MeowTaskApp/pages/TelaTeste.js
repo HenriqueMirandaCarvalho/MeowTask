61b8925b9ffc93f799b30f70eef09bd48cdf7fec
KevinGuga <62311212+KevinGuga@users.noreply.github.com>
2020-03-28 20:39:56 -0300

    testado

--- MeowTaskApp/pages/TelaTeste.js
+++ MeowTaskApp/pages/TelaTeste.js
@@ -5,9 +5,9 @@ export default class TelaInicio extends React.Component {
     render() {
         return (
             <View style={styles}>
-                <View style={{width: 50, height: 50, backgroundColor: 'red'}}><Text>R</Text></View>
-                <View style={{width: 50, height: 50, backgroundColor: 'green'}}><Text>G</Text></View>
-                <View style={{width: 50, height: 50, backgroundColor: 'blue'}}><Text>B</Text></View>
+                <View style={{height: 20, width: "30%", backgroundColor: 'red'}}><Text>R</Text></View>
+                <View style={{height: 20, width: 50, backgroundColor: 'green'}}><Text>G</Text></View>
+                <View style={{height: 20, width: 50, backgroundColor: 'blue'}}><Text>B</Text></View>
             </View>
         );
     }
@@ -15,7 +15,7 @@ export default class TelaInicio extends React.Component {
 
 const styles = {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end'
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
 };
