13441587d171a05d42c2bceb592fcc0affb43790
Henrique Carvalho <henrique.carvalho45@etec.sp.gov.br>
2020-11-24 02:32:31 -0300

    mais recente

--- MeowTaskApp/MeowTask/components/telaArquivos.js
+++ MeowTaskApp/MeowTask/components/telaArquivos.js
@@ -1,30 +1,25 @@
-import React, { useState } from "react";
-import {View, Text, StyleSheet, Image, TouchableNativeFeedback, TouchableOpacity, FlatList, Modal, TouchableWithoutFeedback, TextInput } from "react-native";
-import { Ionicons, AntDesign, FontAwesome5 } from '@expo/vector-icons'; 
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, TouchableNativeFeedback, TouchableOpacity, FlatList, Modal, TouchableWithoutFeedback, TextInput } from "react-native";
+import { Ionicons, AntDesign, FontAwesome5 } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font';
-import {Arquivo} from './arquivo.js';
+import { Arquivo } from './arquivo.js';
 import * as DocumentPicker from 'expo-document-picker';
-
+import * as firebase from 'firebase';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from 'expo-permissions';
 
 const telaArquivos = (props) => {
+    const idTarefa = props.navigation.state.params.idTarefa;
 
-    const [arquivos, setArquivos] = useState([
-        {
-            id: "0",
-            nomeArquivo: "Arquivo 1",
-        },
-        {
-            id: "1",
-            nomeArquivo: "Arquivo 2",
-        },
-    ]);
+    const [arquivos, setArquivos] = useState([]);
     const [guardaTexto, setGuardaTexto] = useState();
     const [guardaId, setGuardaId] = useState();
 
     const [refrescando, setRefrescando] = useState(false);
 
-    function refrescar(){
+    function refrescar() {
         setRefrescando(true);
         alert("olha o refresco!");
     }
@@ -41,8 +36,20 @@ const telaArquivos = (props) => {
         criarItem();
     }
 
-    function baixar(id) {
-        alert("Baixar");
+    function baixar(ref, nome) {
+        ref.getDownloadURL().then(async function (url) {
+            try {
+                const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+                if (status === "granted") {
+                    FileSystem.downloadAsync(url, FileSystem.documentDirectory + nome).then(async ({uri}) => {
+                        const asset = await MediaLibrary.createAssetAsync(uri);
+                        await MediaLibrary.createAlbumAsync("Download", asset, false);
+                    });
+                }
+            } catch (err) {
+                console.warn(err);
+            }
+        });
     }
 
     function toggleModalEditar(id, texto) {
@@ -50,10 +57,10 @@ const telaArquivos = (props) => {
         setGuardaId(id);
         setGuardaTexto(texto);
     }
-    
+
     function editarItem(_id) {
-        const NewData = arquivos.map( item => {
-            if(item.id === _id){
+        const NewData = arquivos.map(item => {
+            if (item.id === _id) {
                 item.nomeArquivo = guardaTexto;
                 return item;
             }
@@ -68,66 +75,100 @@ const telaArquivos = (props) => {
     }
 
     function criarItem() {
-        DocumentPicker.getDocumentAsync();
+        DocumentPicker.getDocumentAsync().then((result) => {
+            if (result.type != "cancel") {
+                urlParaBlob(result.uri).then((obj) => {
+                    firebase.storage().ref().child(idTarefa + "/" + result.name).put(obj);
+                });
+            }
+        });
     }
+
+    function urlParaBlob(url) {
+        return new Promise((resolve, reject) => {
+            const xmlreq = new XMLHttpRequest();
+            xmlreq.onload = () => { resolve(xmlreq.response); }
+            xmlreq.onerror = () => { reject(new Error('Erro ao converter URL para Blob!')); }
+            xmlreq.responseType = 'blob';
+            xmlreq.open("GET", url, true);
+            xmlreq.send(null);
+        });
+    }
+
+    useEffect(() => {
+        setRefrescando(true);
+        const listener = firebase.storage()
+            .ref()
+            .child(idTarefa)
+            .listAll()
+            .then((res) => {
+                let arquivos = [];
+                res.items.forEach((item) => {
+                    arquivos.push({ nome: item.name, ref: item });
+                });
+                setArquivos(arquivos)
+                setRefrescando(false);
+            });
+        return () => listener();
+    }, []);
 
     let [fontsLoaded] = useFonts({
         'Roboto-Light': require('./font/Roboto-Light.ttf'),
         'Roboto-Regular': require('./font/Roboto-Regular.ttf'),
     });
-        
+
     if (!fontsLoaded) {
         return <AppLoading />;
     } else {
-    return (
-        <View style={styles.container}>
-            <View style={styles.cabecalho}>
-                <View style={styles.divSetinha}>
-                    <TouchableOpacity onPress={() => voltar()}>
-                        <Ionicons name="md-arrow-back" size={40} color="#5b5b58" style={styles.setinha}/>
-                    </TouchableOpacity>
+        return (
+            <View style={styles.container}>
+                <View style={styles.cabecalho}>
+                    <View style={styles.divSetinha}>
+                        <TouchableOpacity onPress={() => voltar()}>
+                            <Ionicons name="md-arrow-back" size={40} color="#5b5b58" style={styles.setinha} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.divCabecalho}>
+                        <Text style={styles.titulo}>Arquivos</Text>
+                    </View>
                 </View>
-                <View style={styles.divCabecalho}>
-                    <Text style={styles.titulo}>Arquivos</Text>
-                </View>
-            </View>
-            <View style={styles.conteudo}>
-                <FlatList
-                    data={arquivos}
-                    keyExtractor={item=>item.id}
-                    refreshing={refrescando}
-                    onRefresh={() => refrescar()}
-                    renderItem={({item})=>
-                        <Arquivo 
-                            onPress={() => baixar(item.id)}
-                            onLongPress={() => toggleModalEditar(item.id, item.nomeArquivo)}
-                            texto={item.nomeArquivo}
-                        />
-                    }
-                    style={{width: "100%"}}
-                    ListFooterComponent={
-                        function rodapeLista() {
-                            return (
-                                <View style={styles.rodapeLista}>
-                                    <TouchableOpacity onPress={() => criarItem()} style={styles.botoesRodapeLista}>
-                                        <AntDesign name="pluscircleo" size={60} color="#5b5b58" />
-                                    </TouchableOpacity>
-                                </View>
-                            )
+                <View style={styles.conteudo}>
+                    <FlatList
+                        data={arquivos}
+                        keyExtractor={item => item.id}
+                        refreshing={refrescando}
+                        onRefresh={() => refrescar()}
+                        renderItem={({ item }) =>
+                            <Arquivo
+                                onPress={() => baixar(item.ref, item.nome)}
+                                onLongPress={() => toggleModalEditar(item.ref, item.nome)}
+                                texto={item.nome}
+                            />
                         }
-                    }
-                />
+                        style={{ width: "100%" }}
+                        ListFooterComponent={
+                            function rodapeLista() {
+                                return (
+                                    <View style={styles.rodapeLista}>
+                                        <TouchableOpacity onPress={() => criarItem()} style={styles.botoesRodapeLista}>
+                                            <AntDesign name="pluscircleo" size={60} color="#5b5b58" />
+                                        </TouchableOpacity>
+                                    </View>
+                                )
+                            }
+                        }
+                    />
+                </View>
             </View>
-        </View>
-    );  
-    }  
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-		flexDirection: 'column',
-		backgroundColor: '#EAE6DA',
+        flexDirection: 'column',
+        backgroundColor: '#EAE6DA',
         alignItems: 'center',
     },
     cabecalho: {
@@ -163,7 +204,7 @@ const styles = StyleSheet.create({
     },
     rodapeLista: {
         marginTop: "8%",
-        alignSelf:'stretch',
+        alignSelf: 'stretch',
     },
     botoesRodapeLista: {
         flexDirection: "row",
@@ -195,8 +236,8 @@ const styles = StyleSheet.create({
         alignItems: "center",
         shadowColor: "#000",
         shadowOffset: {
-          width: 0,
-          height: 2
+            width: 0,
+            height: 2
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
