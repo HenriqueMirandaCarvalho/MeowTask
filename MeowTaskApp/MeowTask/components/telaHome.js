34807e2653f26be590658e765231117fdf0908dc
KevinGuga <62311212+KevinGuga@users.noreply.github.com>
2020-11-30 01:43:51 -0300

    Update telaHome.js

--- MeowTaskApp/MeowTask/components/telaHome.js
+++ MeowTaskApp/MeowTask/components/telaHome.js
@@ -9,8 +9,12 @@ import {
     Image,
     Modal,
     TouchableNativeFeedback,
+    TouchableWithoutFeedback,
     ActivityIndicator,
-    Alert
+    Alert,
+    SafeAreaView,
+    FlatList,
+    Dimensions
 } from "react-native";
 
 import { AntDesign, Octicons, Ionicons, FontAwesome, Entypo } from '@expo/vector-icons';
@@ -18,12 +22,19 @@ import { AppLoading } from 'expo';
 import { useFonts } from 'expo-font';
 import { useState } from "react";
 import * as firebase from 'firebase';
+import { Avatar } from './avatar';
+
+const largura = Dimensions.get('window').width;
+const altura = Dimensions.get('window').height;
 
 const telaHome = (props) => {
     const user = firebase.auth().currentUser;
     const [username, setUsername] = useState(firebase.auth().currentUser.displayName);
     const [avatar, setAvatar] = useState(firebase.auth().currentUser.photoURL);
     const [loading, setLoading] = useState(false);
+
+    const [modalAvatarVisivel, setModalAvatarVisivel] = useState(false);
+    const avatares = [require('./img/gato1.png'), require('./img/gato2.png'), require('./img/gato3.png')];
 
     const imagensUsuario = [];
     imagensUsuario.push(require("./img/turquesa10.png"));
@@ -58,6 +69,35 @@ const telaHome = (props) => {
     function toggleModalConfig() {
         props.navigation.navigate("Configuracoes");
     }
+
+    function selecionarAvatar(_index) {
+        if (_index == (avatar-1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function toggleModalAvatar() {
+        if (!modalAvatarVisivel) {
+            setModalAvatarVisivel(true);
+        }
+        else {
+            setModalAvatarVisivel(false);
+
+            // firebase.auth().currentUser.updateProfile({
+            //     displayName: username,
+            //     photoURL: avatar
+            // });
+            // firebase.firestore()
+            //     .collection("Codigos")
+            //     .doc(firebase.auth().currentUser.uid)
+            //     .update({
+            //         nome: username,
+            //         imagem: avatar
+            //     });
+        }
+    }
     
     useEffect(() => {
         const listener = firebase.firestore()
@@ -82,6 +122,41 @@ const telaHome = (props) => {
         return (
             <View style={styles.container}>
                 <Modal
+                    animationType="fade"
+                    transparent={true}
+                    visible={modalAvatarVisivel}
+                    onRequestClose={() => {
+                        toggleModalAvatar();
+                    }}
+                >
+                    <TouchableWithoutFeedback onPress={() => toggleModalAvatar()}>
+                        <View style={styles.overlay} />
+                    </TouchableWithoutFeedback>
+
+                    <View style={styles.centeredView}>
+                        <View>
+                            <View style={styles.modalView}>
+                                <SafeAreaView style={{ width: "92.5%", aspectRatio: 3 }}>
+                                    <FlatList
+                                        data={avatares}
+                                        keyExtractor={(index) => index.toString()}
+                                        horizontal={true}
+                                        renderItem={({ item, index }) =>
+                                            <Avatar
+                                                onPressIn={() => setAvatar(index+1)}
+                                                imagem={item}
+                                                selecionada={selecionarAvatar(index)}
+                                            />
+                                        }
+                                        style={{ backgroundColor: "#69665E" }}
+                                    />
+                                </SafeAreaView>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+
+                <Modal
                     visible={loading}
                     animationType="fade"
                     transparent={true}
@@ -95,10 +170,10 @@ const telaHome = (props) => {
 
                 <View style={styles.cabecalho}>
                     <View style={styles.divAvatar}>
-                        <TouchableOpacity onPress={() => trocarTela(1)}>
+                        <TouchableOpacity onPress={() => toggleModalAvatar()}>
                             <Image source={imagensUsuario[avatar]} style={styles.imagemAvatar}></Image>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => trocarTela(1)}>
+                        <TouchableOpacity onPress={() => toggleModalAvatar()}>
                             <Text style={styles.textoAvatar}>{username}</Text>
                         </TouchableOpacity>
                     </View>
@@ -365,7 +440,40 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
         elevation: 5,
         justifyContent: "center",
-    }
+    },
+    overlay: {
+        position: "absolute",
+        top: 0, // não faço ideia de como o top, right, bottom e left funcionam
+        right: 0, // mas eles fazem o view com absolute ocupar toda a tela
+        bottom: 0,
+        left: 0,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalView: {
+        paddingTop: 0.03*largura,
+        paddingBottom: 0.03*largura,
+        width: 0.80 * largura,
+        backgroundColor: "#c4c4c4",
+        // borderRadius: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    textoTituloModal: {
+        marginTop: "2%",
+        fontFamily: 'Roboto-Light',
+        fontSize: 23,
+    },
 });
 
 export default telaHome;
